a5fd4688fb595c6749edc153144b3057d97b4214
Roshaan Singh <swaggsikh@gmail.com>
2021-04-09 00:05:49 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string="Squawk!")
-  puts "#{string}"
-  "#{string}"
+def parrot(string = "Squawk!")
+  if string == " "
+    puts "#{string}"
+    return "#{string}"
+  else
+    puts "Pretty bird!"
+    return "Pretty bird!"
+  end
 end
